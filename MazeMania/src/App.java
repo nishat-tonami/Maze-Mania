0b288ddac4bc9eb59254ea3e5a1b4638c1a64313
nishat-tonami <nishat.tonami121@gmail.com>
2025-03-29 03:04:00 +0600

    Created game window

--- MazeMania/src/App.java
+++ MazeMania/src/App.java
@@ -1,5 +1,19 @@
+import javax.swing.JFrame;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int rowCnt=21;
+        int columnCnt=19;
+        int tileSize=32;
+        int boardHeight=rowCnt*tileSize;
+        int boardWidth=columnCnt*tileSize;
+
+        JFrame frame=new JFrame("Maze Mania!");
+
+        frame.setVisible(true);
+        frame.setSize(boardWidth,boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
