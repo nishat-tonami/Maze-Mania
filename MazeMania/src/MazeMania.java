e301db07299a282752761e82593ef16d6a34103a
nishat-tonami <nishat.tonami121@gmail.com>
2025-04-02 21:09:48 +0600

    image data type created

--- MazeMania/src/MazeMania.java
+++ MazeMania/src/MazeMania.java
@@ -11,11 +11,36 @@ public class MazeMania extends JPanel {
        private int tileSize=32;
        private int boardHeight=rowCnt*tileSize;
        private int boardWidth=columnCnt*tileSize;
+
+       private Image wallImage;
+       private Image cyanGhost;
+       private Image orangeGhost;
+       private Image pinkGhost;
+       //private Image purpleGhost;
+       private Image redGhost;
+
+       private Image pacmanUp;
+       private Image pacmanDown;
+       private Image pacmanLeft;
+       private Image pacmanRight;
        
 
        MazeMania() {
         setPreferredSize(new Dimension(boardWidth,boardHeight));
         setBackground(Color.darkGray);
+        
+        wallImage=new ImageIcon(getClass().getResource("./wall.png")).getImage();
+        cyanGhost=new ImageIcon(getClass().getResource("./cyan ghost.png")).getImage();
+        orangeGhost=new ImageIcon(getClass().getResource("./orange ghost.png")).getImage();
+        pinkGhost=new ImageIcon(getClass().getResource("./pink ghost.png")).getImage();
+        //purpleGhost=new ImageIcon(getClass().getResource("./purple ghost.png")).getImage();
+        redGhost=new ImageIcon(getClass().getResource("./red ghost.png")).getImage();
 
+        pacmanUp=new ImageIcon(getClass().getResource("./pacman up.png")).getImage();
+        pacmanDown=new ImageIcon(getClass().getResource("./pacman down.png")).getImage();
+        pacmanLeft=new ImageIcon(getClass().getResource("./pacman left.png")).getImage();
+        pacmanRight=new ImageIcon(getClass().getResource("./pacman right.png")).getImage();
+
+        
        }
 }
